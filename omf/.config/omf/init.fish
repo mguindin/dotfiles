004d1ba493f039bf345d0a860386863771f0bd49
Matt Guindin <m@guind.in>
2018-11-12 16:28:42 -0500

    Changing theme to default-dark (I like change, don't blame me)

--- omf/.config/omf/init.fish
+++ omf/.config/omf/init.fish
@@ -1,6 +1,6 @@
 # Base16 Shell
 if status --is-interactive
-    eval sh $HOME/.config/base16-shell/scripts/base16-gruvbox-dark-medium.sh
+    eval sh $HOME/.config/base16-shell/scripts/base16-default-dark.sh
 end
 
 switch (uname -a)
--- vim/.vim/general.vim
+++ vim/.vim/general.vim
@@ -101,7 +101,7 @@ iab Teh The
 " Scrollbar junk
 set guioptions=aAce
 
-colorscheme base16-gruvbox-dark-medium
+colorscheme base16-default-dark
 
 "Show lines numbers
 set number
