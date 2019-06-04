a0dc5480db0c374df05e39fb264bb5e9654b847b
Matt Guindin <m@guind.in>
2019-06-04 11:40:42 -0400

    Moving specific spark stuff to .machine.fish
    - Adding comments to fish config and adding some handy abbreviations

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,4 @@
-# Install fish
+# Install fisher
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
@@ -11,14 +11,18 @@ if status --is-interactive
 end
 
 switch (uname -a)
+  # This is for WSL
   case '*Microsoft*'
     set -xg DOCKER_HOST tcp://127.0.0.1:2375
+  # Linux
   case '*Linux*'
+    # For ubuntu
+    if test -d "/snap/bin"
+      set -xg PATH /snap/bin $PATH
+    end
   case '*Darwin*'
     set -xg GREP_OPTIONS '--color=auto'
     set -xg GREP_COLOR '3;33'
-    # add pyspark
-    set -xg SPARK_HOME /usr/local/Cellar/apache-spark/2.4.0/libexec
 end
 
 set -xg GOPATH $HOME/go
@@ -27,13 +31,18 @@ set -xg LESS '--ignore-case --raw-control-chars'
 set -xg PAGER 'less -R'
 set -xg EDITOR 'vim'
 
+# Set language to en_US
 set -xg LANG en_US.UTF-8
 set -xg LC_CTYPE "en_US.UTF-8"
 set -xg LC_MESSAGES "en_US.UTF-8"
 set -xg LC_COLLATE C
 
+# Set this to not have issues over VPN running spark locally
+set -xg SPARK_LOCAL_IP 127.0.0.1
+
 # fzf / rg
 set -xg FZF_DEFAULT_COMMAND "rg --files"
+
 # set up PATH
 # Add normal binary paths
 set -xg PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /bin $PATH
@@ -49,37 +58,32 @@ if test -d "$HOME/.cargo"
   set -xg PATH $HOME/.cargo/bin $PATH
 end
 
+# If ~/.local/bin is here, add it to PATH
 if test -d "$HOME/.local/bin"
   set -xg PATH $HOME/.local/bin $PATH
 end
 
-# For ubuntu
-if test -d "/snap/bin"
-  set -xg PATH /snap/bin $PATH
-end
-
-
-if test "$HOME/.machine.fish"
-  source $HOME/.machine.fish
-end
-
-if test -d "$SPARK_HOME"
-  # Set this to not have issues over VPN running spark locally
-  set -xg SPARK_LOCAL_IP 127.0.0.1
-  set spark_python $SPARK_HOME/python/lib/py4j-0.10.4-src.zip
-  set -xg PYTHONPATH $spark_python $SPARK_HOME/python $SPARK_HOME/python/build $PYTHONPATH
-end
 
 # Add pyenv, if available
 if test -d "$HOME/.pyenv"
   set -x PYENV_ROOT $HOME/.pyenv
   set -x PYTHON_CONFIGURE_OPTS "--enable-framework"
+  # Set virtualenvs to be located in one place
   set -x WORKON_HOME $HOME/.ve
+  # Set virtualenv projects in one place
   set -x PROJECT_HOME $HOME/p
+  # Make sure virtualfish knows where venvs are
   set -x VIRTUALFISH_HOME $HOME/.ve
+  # Add pyenv root to PATH to access its shims
   set -xg PATH $PYENV_ROOT/bin $PATH
+  # load pyenv and virtualenv-init, etc
   status --is-interactive; and source (pyenv init -|psub); and source (pyenv virtualenv-init -|psub)
   eval (python -m virtualfish compat_aliases auto_activation)
+end
+
+# Add machine-specific stuff if machine.fish is present
+if test "$HOME/.machine.fish"
+  source $HOME/.machine.fish
 end
 
 set -xg MANPATH /usr/local/opt/coreutils/libexec/gnuman /usr/local/opt/gnu-sed/libexec/gnuman $MANPATH
@@ -105,15 +109,17 @@ set -g theme_nerd_fonts no
 set -g theme_show_exit_status yes
 set -g theme_newline_cursor no
 
+# Source direnv if available
 if command -sq direnv
   direnv hook fish | source
 end
 
+# Source kitty if available
 if command -sq kitty
   kitty + complete setup fish | source
 end
 
-# asdf
+# asdf (go/scala/python binary manager)
 if test -d "$HOME/.asdf"
   source ~/.asdf/asdf.fish
 end
