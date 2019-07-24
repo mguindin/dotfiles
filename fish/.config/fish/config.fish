cda53aaa752404b18cb1687d909bf443d77cfedd
Matt Guindin <m@guind.in>
2019-07-23 21:51:05 -0400

    Fixing issue with fish config
    - Adding more fish abbrs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -79,7 +79,6 @@ set -g theme_git_worktree_support no
 set -g theme_title_display_process yes
 set -g theme_title_display_path yes
 set -g theme_title_display_user yes
-set -g theme_display_user no
 set -g theme_date_format "+%a %H:%M:%S"
 set -g theme_title_use_abbreviated_path yes
 set -g theme_nerd_fonts yes
