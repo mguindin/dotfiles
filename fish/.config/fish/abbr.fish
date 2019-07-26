24ca99ef7ff5657c0fbbeb45c6a7c3927163a585
Matt Guindin <m@guind.in>
2019-07-26 15:09:42 -0400

    Moving fish abbrs to different file

---
+++ fish/.config/fish/abbr.fish
@@ -0,0 +1,39 @@
+# abbreviations
+abbr --add --global ... 'cd ../..'
+abbr --add --global arn 'asdf reshim nodejs'
+abbr --add --global b bat
+abbr --add --global be 'bundle exec'
+abbr --add --global dcb 'docker-compose build'
+abbr --add --global dcd 'docker-compose down -v'
+abbr --add --global dcs 'docker-compose stop'
+abbr --add --global dcu 'docker-compose up --build -d'
+abbr --add --global dps 'docker ps'
+abbr --add --global ds 'docker-compose stop'
+abbr --add --global g git
+abbr --add --global gcb 'git checkout origin/master -b '
+abbr --add --global gcm 'git checkout master'
+abbr --add --global gco 'git checkout'
+abbr --add --global gd 'git diff'
+abbr --add --global gp 'git pull'
+abbr --add --global gpm 'git pull origin master'
+abbr --add --global gpull 'git pull'
+abbr --add --global la 'lsd -la'
+abbr --add --global lah 'lsd -lah'
+abbr --add --global ll 'lsd -l'
+abbr --add --global lrt 'lsd -lrt'
+abbr --add --global ls lsd
+abbr --add --global mux tmuxinator
+abbr --add --global nv nvim
+abbr --add --global pa 'pyenv activate'
+abbr --add --global pd 'pyenv deactivate'
+abbr --add --global s 'git status'
+abbr --add --global sf 'source $HOME/.config/fish/config.fish'
+abbr --add --global t tldr
+abbr --add --global ta tn
+abbr --add --global tl 'tmux ls'
+abbr --add --global vfish 'vim $HOME/.config/fish/config.fish'
+abbr --add --global va 'nvim $HOME/.vim/autocommands.vim'
+abbr --add --global vg 'nvim $HOME/.vim/general.vim'
+abbr --add --global vk 'nvim $HOME/.vim/keybindings.vim'
+abbr --add --global vp 'nvim $HOME/.vim/plugins.vim'
+abbr --add --global vs 'nvim -S $HOME/.vim/sessions/session'
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
+set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME $HOME/.config
 # Install fisher
 if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME $HOME/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
@@ -58,43 +58,7 @@ set -xg MANPATH /usr/local/opt/coreutils/libexec/gnuman /usr/local/opt/gnu-sed/l
 
 # global abbreviations (faster than -U universal)
 if status --is-interactive
-    abbr --add --global arn 'asdf reshim nodejs'
-    abbr --add --global b bat
-    abbr --add --global be 'bundle exec'
-    abbr --add --global dcb 'docker-compose build'
-    abbr --add --global dcd 'docker-compose down -v'
-    abbr --add --global dcs 'docker-compose stop'
-    abbr --add --global dcu 'docker-compose up --build -d'
-    abbr --add --global dps 'docker ps'
-    abbr --add --global ds 'docker-compose stop'
-    abbr --add --global g git
-    abbr --add --global gcm 'git checkout master'
-    abbr --add --global gco 'git checkout'
-    abbr --add --global gd 'git diff'
-    abbr --add --global gp 'git pull'
-    abbr --add --global gpm 'git pull origin master'
-    abbr --add --global gpull 'git pull'
-    abbr --add --global la 'lsd -la'
-    abbr --add --global lah 'lsd -lah'
-    abbr --add --global ll 'lsd -l'
-    abbr --add --global lrt 'lsd -lrt'
-    abbr --add --global ls lsd
-    abbr --add --global mux tmuxinator
-    abbr --add --global nv nvim
-    abbr --add --global pa 'pyenv activate'
-    abbr --add --global pd 'pyenv deactivate'
-    abbr --add --global s 'git status'
-    abbr --add --global sf 'source $HOME/.config/fish/config.fish'
-    abbr --add --global t tldr
-    abbr --add --global ta tn
-    abbr --add --global tl 'tmux ls'
-    abbr --add --global vfish 'vim $HOME/.config/fish/config.fish'
-    abbr --add --global va 'nvim $HOME/.vim/autocommands.vim'
-    abbr --add --global vg 'nvim $HOME/.vim/general.vim'
-    abbr --add --global vk 'nvim $HOME/.vim/keybindings.vim'
-    abbr --add --global vp 'nvim $HOME/.vim/plugins.vim'
-    abbr --add --global vs 'nvim -S $HOME/.vim/sessions/session'
-    # etcetera
+  source $XDG_CONFIG_HOME/fish/abbr.fish
 end
 # fish status
 set -g fish_user
